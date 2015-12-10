2172adb71c3bcb33161d89473c72803467b5af23
rizky redjo <it2.axiapro@gmail.com>
2015-12-10 14:39:30 +0700

    email dikasi link konfirmasi pembayaran

--- resources/views/emails/order.blade.php
+++ resources/views/emails/order.blade.php
@@ -3,8 +3,8 @@ Nama : {{$user->fullname}} <br>
 Status Order : {{$status}} <br>
 Anda telah memesan paket {{$package->package_name}} dengan harga Rp. {{number_format($package->price,0,'','.')}} <br>
 
-<?php if ($status=="Belum Lunas") { ?>
-Silahkan melakukan pembayaran, dan mengkonfirmasi order anda. <br>
+<?php if ( $status == "Belum lunas" ) { ?>
+	Silahkan melakukan pembayaran, dan mengkonfirmasi order anda <a href="{{url('confirm-payment')}}"> disini </a>. <br>
 <?php } ?>
 
 <br> Salam hangat, Celebgramme.com
