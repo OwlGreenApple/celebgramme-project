66e8242322f7bac73cea94e92d959c98f0835756
rizky redjo <it2.axiapro@gmail.com>
2015-12-26 15:22:31 +0700

    fixing register

--- app/Http/Controllers/Member/EmailController.php
+++ app/Http/Controllers/Member/EmailController.php
@@ -63,7 +63,7 @@ class EmailController extends Controller
       try {
         $decryptedcode = Crypt::decrypt($cryptedcode);
         $data = json_decode($decryptedcode);
-        $user = User::where("email","=",$data->email)->first;
+        $user = User::where("email","=",$data->email)->first();
         if (!is_null($user)) {
           // Check customer email and status
           if ($user->type == 'not-confirmed'){
