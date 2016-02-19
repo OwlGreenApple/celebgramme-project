61d6bd4ea43a7be1e29dcf54fbb3f6acc2dfb7f4
rizky redjo <it2.axiapro@gmail.com>
2016-02-19 14:19:30 +0700

    fixing email

--- resources/views/emails/create-user.blade.php
+++ resources/views/emails/create-user.blade.php
@@ -1,16 +1,16 @@
-Hi , {{$user->fullname}} <br>
+Hi , {{ucfirst(strtolower($user->fullname)}} <br>
 <br>
 Terima kasih anda sudah membeli service Celebgramme.com <br>
 Berikut ini adalah user credentials anda : <br>
-email : {{$user->email}}<br>
-password : {{$password}}<br>
+<strong>Email :</strong> {{$user->email}}<br>
+<strong>Password :</strong> {{$password}}<br>
 <br>
-Link to login <a href="http://celebgramme.com/celebgramme">-----> Click Link Login Disini <----- </a><br>
+<strong>Link to login </strong><a href="http://celebgramme.com/celebgramme">-----> Click Link Login Disini <----- </a><br>
 <br>
 Silahkan klik Link di atas untuk Login <br>
 dan masukkan username & password anda seperti yg tertulis di atas. <br>
 <br> 
-PS: Anda dapat merubah password anda, setelah anda masuk ke user dashboard. <br>
+<strong>PS:</strong> Anda dapat merubah password anda, setelah anda masuk ke user dashboard. <br>
 <br>
 Salam sukses selalu, <br>
 <br>
