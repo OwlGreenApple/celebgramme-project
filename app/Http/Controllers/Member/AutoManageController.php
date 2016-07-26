7c25ca43f0e131581ac8774c42272808c14ce683
rizky redjo <it2.axiapro@gmail.com>
2016-07-26 16:33:48 +0700

    fixing bug dont follow private users

--- app/Http/Controllers/Member/AutoManageController.php
+++ app/Http/Controllers/Member/AutoManageController.php
@@ -485,7 +485,7 @@ class AutoManageController extends Controller
 	
     if (isset($data['dont_comment_su'])) { $data['dont_comment_su'] = 1; } else { $data['dont_comment_su'] = 0; }
     if (isset($data['dont_follow_su'])) { $data['dont_follow_su'] = 1; } else { $data['dont_follow_su'] = 0; }
-    if (isset($data['dont_follow_pu'])) { $data['dont_follow_pu'] = 1; } else { $data['dont_follow_pu'] = 0; }
+    // if (isset($data['dont_follow_pu'])) { $data['dont_follow_pu'] = 1; } else { $data['dont_follow_pu'] = 0; }
     if (isset($data['unfollow_wdfm'])) { $data['unfollow_wdfm'] = 1; } else { $data['unfollow_wdfm'] = 0; }
 
     $setting_temp->update($data);
