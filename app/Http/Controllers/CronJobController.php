fa3d411194e4f66b4d0cf6aad1cc290ffddf8c29
rizky redjo <it2.axiapro@gmail.com>
2016-08-05 14:41:43 +0700

    testing affiliate

--- app/Http/Controllers/CronJobController.php
+++ app/Http/Controllers/CronJobController.php
@@ -429,7 +429,8 @@ class CronJobController extends Controller
 
 	public function create_user_from_affiliate(){
 		$count_log = 0; $new_user = 0; $adding_time = 0;
-		$datas = DB::connection('mysqlAffiliate')->select("select p.*,u.user_email,u.display_name from wp_af1posts p inner join wp_af1users u on u.id=p.post_author where post_title like 'CLB%' and post_content='' and post_status='publish'");		
+		// $datas = DB::connection('mysqlAffiliate')->select("select p.*,u.user_email,u.display_name from wp_af1posts p inner join wp_af1users u on u.id=p.post_author where post_title like 'CLB%' and post_content='' and post_status='publish'");		
+		$datas = DB::connection('mysqlAffiliate')->select("select p.*,u.user_email,u.display_name from wp_af1posts p inner join wp_af1users u on u.id=p.post_author where post_title like 'CLB%' and post_content='' and post_status='draft'");		
 		// dd($datas);
 		// echo $datas[0]->ID;
 		foreach ($datas as $data) {
@@ -473,9 +474,11 @@ class CronJobController extends Controller
 				$str = 'OCLB'.$dt->format('ymdHi');
 				$order_number = GeneralHelper::autoGenerateID($order, 'no_order', $str, 3, '0');
 				$order->no_order = $order_number;
-				$order->package_manage_id = 31;
+				// $order->package_manage_id = 31;
+				$order->package_manage_id = 34;
 				$order->order_status = "cron dari affiliate";
-				$package = Package::find(31);
+				// $package = Package::find(31);
+				$package = Package::find(34);
 				$order->total = $package->price;
 				$order->user_id = $user->id;
 				$order->save();
