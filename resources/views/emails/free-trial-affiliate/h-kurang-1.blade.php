cdd4bfc6c6da66e5bc9b3d1085e10e617145f7ab
rizky redjo <it2.axiapro@gmail.com>
2016-09-05 17:32:21 +0700

    progress

---
+++ resources/views/emails/free-trial-affiliate/h-kurang-1.blade.php
@@ -0,0 +1,28 @@
+Hi , {{ucfirst(strtolower($user->fullname))}} <br>
+<br>
+<br>
+Sudah sempat Cek Video 7 Rahasia OLShop sukses di Instagram ? <br>
+Ternyata, Jualan di Instagram itu gampang yah. <br>
+Mau dapetin omzet 1-5 juta/ hari ( iya per hari ), <br>
+SEKARANG sudah terlihat di depan mata<br>
+<br>
+Belum nonton ?<br>
+Silahkan Klik Disini ► http://bit.ly/dmusecrets <br>
+Kesempatan BELAJAR GRATIS, kapan lagi ? <br>
+<br>
+Apabila bermanfaat, jangan lupa di share Videonya <br>
+#indahnya berbagi <br>
+<br>
+Sekalian, mau mengingatkan juga, <br>
+Masa Trial Kakak besok akan habis. <br>
+Jika mau menambahkan waktu berlangganan <br>
+silahkan Klik langsung disini <br>
+< link wuoy page affiliate > <br>
+<br>
+PS: Belajar dari cerita sukses teman2 yang jualan di Instagram dilink ini <br>
+https://celebgramme.com/category/testimonial/ <br>
+<br>
+salam hangat, <br>
+<br>
+Michael Sugiharto<br>
+Celebgramme.com
