a7cc466717e7989797c26620e55b0b9bddb91f7c
rizky redjo <it2.axiapro@gmail.com>
2016-09-06 17:14:41 +0700

    test

--- app/Http/Controllers/CronJobController.php
+++ app/Http/Controllers/CronJobController.php
@@ -550,6 +550,7 @@ class CronJobController extends Controller
 				// $package = Package::find(31);
 				// $package = Package::find(34);
 				$data_meta = DB::connection('mysqlAffiliate')->select("select meta_value from wp_af1postmeta where meta_key='price' and post_id = ".$data->ID);		
+				dd($data);
 				dd($data_meta);
 				echo "<br> asd".$data_meta[0]->meta_value;
 				$package = Package::select(DB::raw("ABS( price - ".$data_meta[0]->meta_value." ) AS distance"))
