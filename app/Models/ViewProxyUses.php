65b8b968eb38b960b523d29ed57de31d1ea86580
rizky redjo <it2.axiapro@gmail.com>
2016-11-04 16:17:36 +0700

    testing

---
+++ app/Models/ViewProxyUses.php
@@ -0,0 +1,16 @@
+<?php namespace Celebgramme\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class ViewProxyUses extends Model {
+
+	protected $table = 'proxy_uses_total';
+    public $timestamps = false;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [ ];
+}
