285b67f9f232b1022e81afaa059b329634713ecc
rizky redjo <it2.axiapro@gmail.com>
2017-03-31 13:32:24 +0700

    setting_id

---
+++ database/migrations/2017_03_31_132920_add_settingid_to_usersettings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSettingidToUsersettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_settings', function (Blueprint $table) {
+            $table->integer('setting_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user_settings', function (Blueprint $table) {
+            //
+        });
+    }
+}
