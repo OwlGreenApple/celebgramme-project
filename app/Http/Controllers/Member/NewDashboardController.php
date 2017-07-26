e6b8a4b481f84afe5f4fbcea980eff90cf6f79c0
rizky redjo <it2.axiapro@gmail.com>
2017-07-26 11:16:36 +0700

    testing

--- app/Http/Controllers/Member/NewDashboardController.php
+++ app/Http/Controllers/Member/NewDashboardController.php
@@ -618,15 +618,10 @@ class NewDashboardController extends Controller
 				$arr_inbox = (array) $arr_inbox;
 				if (Request::input("is_sort") == "1") {
 					usort($arr_inbox, function($a, $b) {
-						// if ( ($a['status_new_message'] == $b['status_new_message']) && ($b['date_message1'] >= $a['date_message1']) ) {
-							// return $b['date_message1'] - $a['date_message1'];
-						// }
-						return $b['status_new_message'] - $a['status_new_message'];
-					});
-					usort($arr_inbox, function($a, $b) {
-						if ($a['status_new_message'] == $b['status_new_message']) {
+						if ( ($a['status_new_message'] == $b['status_new_message']) && ($b['date_message1'] >= $a['date_message1']) ) {
 							return $b['date_message1'] - $a['date_message1'];
 						}
+						return $b['status_new_message'] - $a['status_new_message'];
 					});
 				}
 				$total_data = count($arr_inbox);
