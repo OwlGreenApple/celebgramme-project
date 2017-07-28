deab3fbdf62aefacf1f8acc07f43a3cf86e03a6b
rizky redjo <it2.axiapro@gmail.com>
2017-07-28 16:24:23 +0700

    fixing

--- resources/views/new-dashboard/DM-req.blade.php
+++ resources/views/new-dashboard/DM-req.blade.php
@@ -7,13 +7,13 @@
 																		$date_message = substr($data_arr->getItems()[0]->getTimestamp(),0,10);
 																		$text_message = $data_arr->getItems()[0]->getText();
 																		if (strlen($text_message)>=42) {
-																			$text_message = substr($text_message,0,115)." ...";
+																			$text_message = substr($text_message,0,60)." ...";
 																		}
 															?>
 															<div class="row">
-																<div style="padding:10px;" class="col-md-5 col-sm-12 col-xs-12 bg-white br-6 same-height">
-																	<div style="margin-top:10px;" class="col-md-2 col-sm-2 col-xs-2 ">
-																		<img src="{{$data_arr->getUsers()[0]->getProfilePicUrl()}}" class="img-circle" style="width:50px;height:50px;">
+																<div style="padding:5px;" class="col-md-5 col-sm-12 col-xs-12 bg-white br-6 same-height">
+																	<div style="margin-top:7px;" class="col-md-2 col-sm-2 col-xs-2 ">
+																		<img src="{{$data_arr->getUsers()[0]->getProfilePicUrl()}}" class="img-circle" style="width:35px;height:35px;">
 																	</div>
 																	<div style="" class="col-md-10 col-sm-10 col-xs-10 ">
 																		<h3 style="color:#333;font-weight:200;">
@@ -25,21 +25,21 @@
 																<div style="" class="col-md-7 col-sm-12 col-xs-12">
 																	<div class="row">
 																		<div class="col-md-4 col-sm-4 col-xs-4 br-6">
-																			<div style="padding:10px;" class="bg-white br-6 text-center same-height">
-																				<b class="text-primary" style="margin-top:25px;display:block">
+																			<div style="padding:5px;" class="bg-white br-6 text-center same-height">
+																				<b class="text-primary" style="margin-top:7px;display:block">
 																					{{date("l", $date_message)}}<br>{{date("Y-m-d", $date_message)}}
 																				</b>
 																			</div>
 																		</div>
 																		<div class="col-md-4 col-sm-4 col-xs-4 br-6">
-																			<div style="padding:10px;cursor:pointer;" class="bgGreenLight text-center button-accept-request same-height" data-thread-id="{{$data_arr->getThreadId()}}" data-pic="{{$data_arr->getUsers()[0]->getProfilePicId()}}" href="#chat-all" data-toggle="tab"  data-username="{{$data_arr->getUsers()[0]->getUsername()}}" >
-																				<i class="fa fa-check fa-2x" style="margin-top:25px;display:block"></i>
+																			<div style="padding:5px;cursor:pointer;" class="bgGreenLight text-center button-accept-request same-height" data-thread-id="{{$data_arr->getThreadId()}}" data-pic="{{$data_arr->getUsers()[0]->getProfilePicId()}}" href="#chat-all" data-toggle="tab"  data-username="{{$data_arr->getUsers()[0]->getUsername()}}" >
+																				<i class="fa fa-check fa-2x" style="margin-top:7px;display:block"></i>
 																				<b class="text-white">Accept</b>
 																			</div>
 																		</div>
 																		<div class="col-md-4 col-sm-4 col-xs-4 br-6">
-																			<div style="padding:10px;cursor:pointer;" class="bg-red text-center button-decline-request same-height" data-toggle="modal" data-target="#confirm-decline" data-thread-id="{{$data_arr->getThreadId()}}">
-																				<i class="fa fa-times fa-2x" style="margin-top:25px;display:block"></i>
+																			<div style="padding:5px;cursor:pointer;" class="bg-red text-center button-decline-request same-height" data-toggle="modal" data-target="#confirm-decline" data-thread-id="{{$data_arr->getThreadId()}}">
+																				<i class="fa fa-times fa-2x" style="margin-top:7px;display:block"></i>
 																				<b class="text-white">Decline</b>
 																			</div>
 																		</div>
