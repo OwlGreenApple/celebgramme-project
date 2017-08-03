3711ddab94ded4d9b140949bb2673ea78079d097
rizky redjo <it2.axiapro@gmail.com>
2017-08-03 22:47:23 +0700

    fixing

--- resources/views/new-dashboard/chat-all.blade.php
+++ resources/views/new-dashboard/chat-all.blade.php
@@ -12,66 +12,68 @@
 		</div>
 	</div>
   <div class="row" style="overflow: auto; max-height: 500px;">
-		<?php 
-			// $items = array_reverse($chatAll->thread->items);
-			$items = array_reverse($chatAll->getThread()->getItems());
-			foreach($items as $data) { ?>
-				<div style="min-height:50px;overflow:hidden;">
-				<div class="col-md-1 col-sm-1 col-xs-1">
-				&nbsp
-				</div>	
-				<div class="col-md-9 col-sm-9 col-xs-9">
-					<div class="<?php if ($chatAll->thread->getUsers()[0]->getPk() != $data->getUserId() ) { echo "fr"; } else { echo "fl"; } ?> well" style="padding:10px; <?php if ($chatAll->thread->getUsers()[0]->getPk() == $data->getUserId() ) { echo "background-color:#fff;"; }?>">
-						<?php 
-							if (strtolower($data->getItemType()) == "text" ) {
-								echo $data->getText();
-							}
-							// else if (strtolower($data->getItemType()) == "reel_share" ) {
-							else {	
-								$url_img = "";
-								$message = "";
-								if (strtolower($data->getItemType()) == "reel_share" ) {
-									$shareData = $data->getReelShare();
-								}
-								else if (strtolower($data->getItemType()) == "media_share" ) {
-									$shareData = $data->getMediaShare();
-								}
-								else if (strtolower($data->getItemType()) == "raven_media" ) {
-									$shareData = $data->getRavenMedia();
-								}
-								
-								if (!is_null($shareData)) {
-									if (!is_null($shareData->getMedia())) {
-										if (!is_null($shareData->getMedia()->getId())) {
-											if (!is_null($i->media->getInfo($shareData->getMedia()->getId())->getItems())) {
-												$res_url = $i->media->getInfo($shareData->getMedia()->getId())->getItems()[0]->getImageVersions2()->getCandidates()[0]->getUrl();
-												if (!is_null($res_url)) {
-													$url_img = $res_url;
-												}
-											}
-										}
-									}
-									if (!is_null($data->getText())) {
-										$message = $data->getText();
-									}
-									// $message = $shareData->getText();
-								}
-								
-								if ($url_img <> "") {
-									echo '<img class="img-responsive" src="'.$url_img.'" style="width:200px;height:100%;"><br>'.$message;
-								} else {
-									echo $message;
-								}
-							}
-						?>
-					</div>
-				</div>
-				<div class="col-md-2 col-sm-2 col-xs-2">
-				</div>
-				</div>
-				
-		<?php } ?>
-  </div>	
+  <?php 
+   // $items = array_reverse($chatAll->thread->items);
+   $items = array_reverse($chatAll->getThread()->getItems());
+   foreach($items as $data) { ?>
+    <div style="min-height:50px;overflow:hidden;">
+    <div class="col-md-1 col-sm-1 col-xs-1">
+    &nbsp
+    </div> 
+    <div class="col-md-9 col-sm-9 col-xs-9">
+     <div class="<?php if ($chatAll->getThread()->getUsers()[0]->getPk() != $data->getUserId() ) { echo "fr"; } else { echo "fl"; } ?> well" style="padding:10px; <?php if ($chatAll->getThread()->getUsers()[0]->getPk() == $data->getUserId() ) { echo "background-color:#fff;"; }?>">
+      <?php 
+       if (strtolower($data->getItemType()) == "text" ) {
+        echo $data->getText();
+       }
+       // else if (strtolower($data->getItemType()) == "reel_share" ) {
+       else { 
+        $url_img = "";
+        $message = "";
+        if (strtolower($data->getItemType()) == "reel_share" ) {
+         $shareData = $data->getReelShare();
+        }
+        else if (strtolower($data->getItemType()) == "media_share" ) {
+         $shareData = $data->getMediaShare();
+        }
+        else if (strtolower($data->getItemType()) == "raven_media" ) {
+         $shareData = $data->getRavenMedia();
+        }
+        
+        //harus ada pengecekan klo carousel atau image biasa, klo carousel diambil gambar yang pertama
+        
+        if (!is_null($shareData)) {
+         if (!is_null($shareData->getMedia())) {
+          if (!is_null($shareData->getMedia()->getId())) {
+           if (!is_null($i->media->getInfo($shareData->getMedia()->getId())->getItems())) {
+            $res_url = $i->media->getInfo($shareData->getMedia()->getId())->getItems()[0]->getImageVersions2()->getCandidates()[0]->getUrl();
+            if (!is_null($res_url)) {
+             $url_img = $res_url;
+            }
+           }
+          }
+         }
+         if (!is_null($data->getText())) {
+          $message = $data->getText();
+         }
+         // $message = $shareData->getText();
+        }
+        
+        if ($url_img <> "") {
+         echo '<img class="img-responsive" src="'.$url_img.'" style="width:200px;height:100%;"><br>'.$message;
+        } else {
+         echo $message;
+        }
+       }
+      ?>
+     </div>
+    </div>
+    <div class="col-md-2 col-sm-2 col-xs-2">
+    </div>
+    </div>
+    
+  <?php } ?>
+  </div>
 	<div class="row">
 		<div class="col-md-1 col-sm-1 col-xs-1">
 		</div>	
