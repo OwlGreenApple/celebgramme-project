b98b57eb591d384a26327f43ae07e3f4de94e1c2
rizky redjo <it2.axiapro@gmail.com>
2017-08-04 14:18:29 +0700

    fixing

--- resources/views/new-dashboard/chat-all.blade.php
+++ resources/views/new-dashboard/chat-all.blade.php
@@ -72,8 +72,10 @@
 										//dari image carousel 
 										$res_url = $mediaShare->getCarouselMedia();
 										if (!is_null($res_url)) {
-											if (!is_null($res_url[0]->getCandidates())) {
-												$url_img = $res_url[0]->getCandidates()[0]->getUrl();
+											if (!is_null($res_url[0]->getImageVersions2())) {
+												if (!is_null($res_url[0]->getImageVersions2()->getCandidates())) {
+													$url_img = $res_url[0]->getImageVersions2()->getCandidates()[0]->getUrl();
+												}
 											}
 										}
 									}
