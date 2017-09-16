d20454fedba64042ab5bbda424a8e0086dc26c79
rizky redjo <celebgramme.dev@gmail.com>
2017-09-16 12:10:32 +0700

    pixel fb ads dihalaman depan

--- resources/views/check-out.blade.php
+++ resources/views/check-out.blade.php
@@ -8,6 +8,30 @@
       <link href="{{ asset('/css/bootstrap-theme.min.css') }}" rel="stylesheet">
       <link href="{{ asset('/css/package.css') }}" rel="stylesheet">
       <script type="text/javascript" src="{{ asset('/js/jquery-1.11.3.js') }}"></script>
+			<!-- Facebook Pixel Code -->
+			<script>
+			!function(f,b,e,v,n,t,s)
+			{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+			n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+			if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+			n.queue=[];t=b.createElement(e);t.async=!0;
+			t.src=v;s=b.getElementsByTagName(e)[0];
+			s.parentNode.insertBefore(t,s)}(window,document,'script',
+			'https://connect.facebook.net/en_US/fbevents.js');
+			 fbq('init', '925095610957570'); 
+			fbq('track', 'PageView');
+			</script>
+			<noscript>
+			 <img height="1" width="1" 
+			src="https://www.facebook.com/tr?id=925095610957570&ev=PageView
+			&noscript=1"/>
+			</noscript>
+			<!-- End Facebook Pixel Code -->
+
+			<script>
+				fbq('track', 'AddPaymentInfo');
+			</script>
+			
       <script type="text/javascript">
         function commaSeparateNumber(val){
           while (/(\d+)(\d{3})/.test(val.toString())){
@@ -77,6 +101,8 @@
               // alert("Silahkan pilih salah satu paket. paket daily likes atau paket auto manage");
               e.preventDefault();
             }
+						fbq('track', 'Purchase', {value: total, currency: 'IDR'});
+
           });
 
         });
