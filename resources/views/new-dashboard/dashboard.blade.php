ca5579feff0e44344dd87e9c8126fae27f62838f
rizky redjo <celebgramme.dev@gmail.com>
2017-09-26 14:46:32 +0700

    fixing

--- resources/views/new-dashboard/dashboard.blade.php
+++ resources/views/new-dashboard/dashboard.blade.php
@@ -288,6 +288,7 @@
                 var data = jQuery.parseJSON(result);
                 $("#alert").show();
                 $("#alert").html(data.message);
+								window.scrollTo(0, 0);
                 if(data.type=='success')
                 {
                   $("#alert").addClass('alert-success');
