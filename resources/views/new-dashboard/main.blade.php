37081227ee97b631dac87e7d69172aa0ac553da1
rizky redjo <celebgramme.dev@gmail.com>
2017-11-24 23:08:12 +0700

    fixing

--- resources/views/new-dashboard/main.blade.php
+++ resources/views/new-dashboard/main.blade.php
@@ -196,7 +196,7 @@
             <div class="legal">
                 <div class="copyright">
                     &copy; 2017 <a href="{{url('')}}">Celebgramme</a>.
-                    <b>Version: </b> 3.5
+                    <b>Version: </b> 3.6
                 </div>
             </div>
             <!-- #Footer -->
@@ -276,7 +276,7 @@
             <div class="legal">
                 <div class="copyright">
                     &copy; 2017 <a href="{{url('')}}">Celebgramme</a>.
-                    <b>Version: </b> 3.5
+                    <b>Version: </b> 3.6
                 </div>
             </div>
             <!-- #Footer -->
