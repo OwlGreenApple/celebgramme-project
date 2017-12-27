e04b28846081f44f0c6babde005e2cda06d3a10f
rizky redjo <celebgramme.dev@gmail.com>
2017-12-27 15:15:40 +0700

    trying fixing proxy remove

--- app/Http/Controllers/CronJobController.php
+++ app/Http/Controllers/CronJobController.php
@@ -834,7 +834,7 @@ class CronJobController extends Controller
 								->where("settings.type",'=','temp')
 								->where("proxy_id","!=",0)
 								->where("settings.running_time","<",$dt->toDateTimeString())
-								->where("settings.start_time","<>",0)
+								->where("settings.start_time","<>","0000-00-00 00:00:00")
 								->where(function ($query) {
 									$query->where("settings.status","=","stopped")
 												->orWhere("settings.status","=","deleted")
