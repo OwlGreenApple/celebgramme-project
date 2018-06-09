33eda204c31e29877b449534598305b8868f426d
rizky redjo <celebgramme.dev@gmail.com>
2018-06-09 14:30:39 +0700

    new code for userguiding purpose

--- resources/views/new-dashboard/main.blade.php
+++ resources/views/new-dashboard/main.blade.php
@@ -64,19 +64,18 @@
 			$(document).ready(function(){
 				$("#div-loading").hide();
 			});
-			(function (u, s, e, r, g) { 
-					u[r] = u[r] || [];
-					u[r].push({
-						'ug.start': new Date().getTime(), event: 'embed.js',
-					});
-					var f = s.getElementsByTagName(e)[0],
-							j = s.createElement(e);
-					j.async = true;
-					j.src = 'https://static.userguiding.com/media/user-guiding-'
-					 + g + '-embedded.js';
-					f.parentNode.insertBefore(j, f);
-			})(window, document, 'script', 'userGuidingLayer', '62128164ID');
-			
+(function (u, s, e, r, g) { 
+    u[r] = u[r] || [];
+    u[r].push({
+      'ug.start': new Date().getTime(), event: 'embed.js',
+    });
+    var f = s.getElementsByTagName(e)[0],
+        j = s.createElement(e);
+    j.async = true;
+    j.src = 'https://static.userguiding.com/media/user-guiding-'
+     + g + '-embedded.js';
+    f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'userGuidingLayer', '84184911ID');
 		</script>
 		
 </head>
