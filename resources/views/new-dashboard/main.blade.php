16e81c7ef9c16c8a19e5b73baf84bc4b7bf0cd11
rizky redjo <celebgramme.dev@gmail.com>
2018-06-12 14:05:18 +0700

    ke logout2 sendiri

--- resources/views/new-dashboard/main.blade.php
+++ resources/views/new-dashboard/main.blade.php
@@ -2,21 +2,6 @@
 <html>
 
 <head>
-<script>
-(function (u, s, e, r, g) { 
-    u[r] = u[r] || [];
-    u[r].push({
-      'ug.start': new Date().getTime(), event: 'embed.js',
-    });
-    var f = s.getElementsByTagName(e)[0],
-        j = s.createElement(e);
-    j.async = true;
-    j.src = 'https://static.userguiding.com/media/user-guiding-'
-     + g + '-embedded.js';
-    f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'userGuidingLayer', '47185407ID');
-</script>
-
     <meta charset="UTF-8">
 		<meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
